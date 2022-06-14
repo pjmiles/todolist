d98b3f42822c014e5364b6837ed79d5fd2f53ee2
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-06-14 16:55:44 +0100

    schema created

--- api/server.js
+++ api/server.js
@@ -1,14 +1,23 @@
 require("dotenv").config();
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
-const db = require('./config/mongoose')
+const db = require("./config/mongoose");
+const port = 4000;
+const Todo = require('./model/task')
+
 
 const app = express();
-
 app.use(express.json());
 
 
 
 
-app.listen(4000, () => console.log("Server started on port 4000"));
+
+
+
+
+
+app.listen(process.env.PORT || port, () =>
+  console.log(`Server started on port: ${port}`)
+);
